ebb50797891a50034145acafe58182f6d2c74b23
Ethan <ethanchen2009@hotmail.com>
2022-12-11 22:48:44 +0800

    Fix Room instance method removePlayer and refactor Model instance and static method

--- models/playerModel.js
+++ models/playerModel.js
@@ -3,37 +3,12 @@ const Room = require("./roomModel");
 
 const Schema = mongoose.Schema;
 
-const playerSchema = new Schema(
-  {
-    playerID: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    room: { type: Schema.Types.ObjectId, ref: "Room" },
-  },
-  { timestamps: true }
-);
-
-playerSchema.statics.findOneOrCreate = async function (
-  condition,
-  defaultValue
-) {
-  const player = await this.findOne(condition);
-  if (player) {
-    return player;
-  } else {
-    return this.create(defaultValue);
-  }
-};
-
 /**
  * Join a room instance that the `player` instance might be in.
  * @param roomID The id of a room.
  */
-playerSchema.methods.joinRoom = async function (roomID) {
+const joinRoom = async function (roomID) {
   const roomInstance = await Room.findOne({ roomID });
-  console.log(`player.joinRoom.roomInstance : ${roomInstance}`);
   if (!roomInstance) {
     throw `Room ${roomID} is not found! Please try another room!`;
   }
@@ -50,12 +25,13 @@ playerSchema.methods.joinRoom = async function (roomID) {
   await roomInstance.addPlayer(this);
   await this.save();
   console.log(`player.joinRoom: player = ${this}`);
+  console.log(`player.joinRoom: roomInstance = ${roomInstance}`);
 };
 
 /**
  * Leave any room that the player might be in.
  */
-playerSchema.methods.leaveRoom = async function () {
+const leaveRoom = async function () {
   // A player can only leave a room if they are already in a room.
   if (!this.room) {
     console.log(`Player ${this.playerID} is not in any room!`);
@@ -71,5 +47,35 @@ playerSchema.methods.leaveRoom = async function () {
   await this.save();
   if (oldRoom.players.length === 0)
     await Room.deleteOne({ roomID: oldRoom.roomID });
+  console.log(`player.leaveRoom: player = ${this}`);
+  console.log(`player.leaveRoom: roomInstance = ${oldRoom}`);
 };
+
+/**
+ * Returns a player instance if the player exists in the database
+ * or create a new player instance if it doesn't exists.
+ * @param condition A condition object.
+ * @param defaultValue The attributes for creating a player instance if this player isn't in database yet.
+ */
+const findOneOrCreate = async function (condition, defaultValue) {
+  const player = await this.findOne(condition);
+  if (player) {
+    return player;
+  } else {
+    return this.create(defaultValue);
+  }
+};
+
+const playerSchema = new Schema(
+  {
+    playerID: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    room: { type: Schema.Types.ObjectId, ref: "Room" },
+  },
+  { methods: { joinRoom, leaveRoom }, statics: { findOneOrCreate } }
+);
+
 module.exports = mongoose.model("Player", playerSchema);
--- models/roomModel.js
+++ models/roomModel.js
@@ -2,6 +2,40 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
+/**
+ * Add a player instance into this room instance.
+ * @param playerInstance An instance of the Player model.
+ */
+const addPlayer = async function (playerInstance) {
+  console.log(
+    `Adding player ${playerInstance.playerID} to room ${this.roomID}`
+  );
+  const numPlayerInRoom = this.players.length;
+  const roomHasNoSpace = numPlayerInRoom >= 4;
+  if (roomHasNoSpace) {
+    throw `Room ${room.roomID} is full! Please try another room!`;
+  }
+  this.players.push(playerInstance);
+  await this.save();
+};
+
+/**
+ * Remove a player instance from this room instance.
+ * @param playerInstance An instance of the Player model.
+ */
+const removePlayer = async function (playerInstance) {
+  const thisRoom = await mongoose
+    .model("Room")
+    .findOne({ roomID: this.roomID })
+    .populate("players");
+
+  const updatedPlayers = thisRoom.players.filter(
+    (plyr) => plyr.playerID !== playerInstance.playerID
+  );
+  this.players = updatedPlayers;
+  await this.save();
+};
+
 const roomSchema = new Schema(
   {
     roomID: {
@@ -15,31 +49,13 @@ const roomSchema = new Schema(
     players: [{ type: Schema.Types.ObjectId, ref: "Player" }],
     readyPlayers: [{ type: Schema.Types.ObjectId, ref: "Player" }],
   },
-  { timestamps: true }
-);
-
-roomSchema.methods.addPlayer = async function (playerInstance) {
-  console.log(
-    `Adding player ${playerInstance.playerID} to room ${this.roomID}`
-  );
-  const numPlayerInRoom = this.players.length;
-  const roomHasNoSpace = numPlayerInRoom >= 4;
-  if (roomHasNoSpace) {
-    throw `Room ${room.roomID} is full! Please try another room!`;
+  {
+    methods: {
+      addPlayer,
+      removePlayer,
+    },
   }
-  this.players.push(playerInstance);
-  await this.save();
-};
-
-roomSchema.methods.removePlayer = async function (playerInstance) {
-  console.log(`Removing player ${playerInstance} from room ${this}`);
-  this.populate("players");
-  const newPlayers = this.players.filter(
-    (plyr) => plyr.playerID !== playerInstance.playerID
-  );
-  this.players = newPlayers;
-  await this.save();
-};
+);
 
 roomSchema.methods.isEqual = function (roomInstance) {
   return this._id.equals(roomInstance._id);
